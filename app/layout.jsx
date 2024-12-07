2065203e6873e95fdf9135239bdf69d9e9657ed5
Blaise Mugisha <blaimug@gmail.com>
2024-12-07 03:49:27 +0200

    updated metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,18 @@
-import { Geist } from 'next/font/google'
-import Script from 'next/script'
-import './globals.css'
-import { Navigation } from '@/components/navigation'
-import { Footer } from '@/components/footer'
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { Geist } from "next/font/google";
+import Script from "next/script";
+import "./globals.css";
+import { Navigation } from "@/components/navigation";
+import { Footer } from "@/components/footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
-const geist = Geist({ subsets: ['latin'] })
+const geist = Geist({ subsets: ["latin"] });
 
 export const metadata = {
   verification: { google: "x2Pjbm095gMdhic33fpc3Hv1D1oKusFqOiycAJi_3sA" },
-  title: 'Zeal - Congregation Assistant App',
-  description: 'Schedule Christian Life & Ministry meeting parts, Public Talk assignments, Duties, Territories, Cart Schedule, Field Service Reports and more.',
-  metadataBase: new URL('https://zeal.rw'),
+  title: "Zeal - Congregation Assistant App",
+  description:
+    "Schedule Christian Life & Ministry meeting parts, Public Talk assignments, Duties, Territories, Cart Schedule, Field Service Reports and more.",
+  metadataBase: new URL("https://zeal.rw"),
   keywords: [
     "jw",
     "Jehovah's Witnesses",
@@ -23,43 +24,48 @@ export const metadata = {
     "jwcart",
     "public witnessing",
     "zeal jw",
+    "s-140",
+    "jw meetings",
+    "hourglass",
+    "jw library",
+    "jw language",
+    "zeal",
   ],
   openGraph: {
-    title: 'Zeal - Congregation Assistant App',
-    description: 'Schedule Christian Life & Ministry meeting parts, Public Talk assignments, Duties, Territories, Cart Schedule, Field Service Reports and more.',
-    url: 'https://zeal.rw',
-    siteName: 'Zeal',
+    title: "Zeal - Congregation Assistant App",
+    description:
+      "Schedule Christian Life & Ministry meeting parts, Public Talk assignments, Duties, Territories, Cart Schedule, Field Service Reports and more.",
+    url: "https://zeal.rw",
+    siteName: "Zeal",
     images: [
       {
-        url: '/og-image.png',
+        url: "/icon.png",
         width: 1200,
         height: 630,
       },
     ],
-    locale: 'en_US',
-    type: 'website',
+    locale: "en_US",
+    type: "website",
   },
   twitter: {
-    card: 'summary_large_image',
-    title: 'Zeal - Congregation Assistant App',
-    description: 'Schedule Christian Life & Ministry meeting parts, Public Talk assignments, Duties, Territories, Cart Schedule, Field Service Reports and more.',
-    images: ['/og-image.png'],
+    card: "summary_large_image",
+    title: "Zeal - Congregation Assistant App",
+    description:
+      "Schedule Christian Life & Ministry meeting parts, Public Talk assignments, Duties, Territories, Cart Schedule, Field Service Reports and more.",
+    images: ["/icon.png"],
   },
-
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <head>
-      </head>
+      <head></head>
       <body className={`${geist.className} antialiased`}>
         <Navigation />
         <main>{children}</main>
         <Footer />
       </body>
-      <GoogleAnalytics gaId='G-NTTM68XK10' />
+      <GoogleAnalytics gaId="G-NTTM68XK10" />
     </html>
-  )
+  );
 }
-
